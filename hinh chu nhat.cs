4cff92a0e4e929481ba132c92349ddc883364f98
ADMIN <ADMIN@LAB1-MAY04>
2024-06-17 14:16:35 +0700

    Add project files.

---
+++ hinh chu nhat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_HinhChuNhat_Class
+{
+    internal class hinh_chu_nhat
+    {
+        public double chieucao;
+        public double chieurong;
+
+
+        public double TinhDienTich()
+        { return chieucao * chieurong;
+        }
+        public double tinhchuvi()
+        {
+            return (chieucao * chieurong) * 2;
+        }
+    }
+}
